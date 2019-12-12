3661163f4b1267006ce4118d9e0673016c65ea3b
YANNI <yanniflambas@hotmail.com>
2019-12-12 22:07:57 +1100

    commiting project updates wk3day4 2nd

--- js/main.js
+++ js/main.js
@@ -1,41 +1,4 @@
-//function hidetext(//Tic Tac Toe v0.1 Draft of work planning
-//
-//End Goals - Overall - Minimum Work
-//
-// 1) A working game, built by you, hosted somewhere on the internet
-// 2)A link to your hosted working game in the URL section of your Github repo
-// 3) A git repository hosted on Github, with a link to your hosted game, and frequent commits dating back to the very beginning of the project
-// 4) A readme.md file with explanations of the technologies used, the approach taken, installation instructions, unsolved problems, etc.
-// 5) On Friday of project week everyone individually presents their project to the class, with an overview of the app and a brief code tour of the highlights.
-//
-// //End Goals - Functionality - The app must:
-// 1) Render a game board in the browser √
-// 2) Switch turns between X and O (or whichever markers you select); your game should prevent users from playing a turn into a square that is already occupied
-// 3) Visually display which side won if a player gets three in a row or show a draw/"cat’s game" if neither wins
-// 4) Include separate HTML / CSS / JavaScript files
-// 5) Stick with KISS (Keep It Simple Stupid) and DRY (Don't Repeat Yourself) principles
-// 6) Use Javascript with jQuery for DOM manipulation
-// 7) Deploy your game online, where the rest of the world can access it
-// 8) Use semantic markup for HTML and CSS (adhere to best practices)
-
-// //End Goals - Bonus - Extra Credit:
-// Keep track of multiple game rounds with a win counter
-// Allow players to customize their tokens (X, O, name, picture, etc)
-// Get inventive with your styling, e.g. use hover effects or animations to spiff things up
-// Use LocalStorage to persist data locally to allow games to continue after page refresh or loss of internet connectivity
-// Support custom board sizes: default is 3x3 but you could allow users to choose a larger board
-// Support networked multiplayer: https://www.firebase.com/ has a nice quickstart guide
-// Create an AI opponent: teach Javascript to play an unbeatable game against you
-// Start by implementing a few simple rules which can be easily checked and are always good moves, such as "always take the center square if it's available" - you can google these rules for yourself
-// You can build in as many AI player rules as you like but you'll quickly end up with a longwinded list of if-else-if statements. To make a truly unbeatable AI opponent you'll need to look into implementing an algorithm like MiniMax - for advanced/bold students only!
-// Start again by implementing a totally different game: Try Checkers, or Battleships.... or Chess (if you're feeling very brave)
-// Make a Vue.js version of your game, instead of jQuery, and compare the two...
-
-
-
-
-
-
+//Tic Tac Toe v0.1 Draft of work planning
 
 $(document).ready( function(){
   // Variables pre set up for easy personal reference.
@@ -90,7 +53,8 @@ $(document).ready( function(){
         player1[boxIndex] = $(this)[0].id; // Change player1's array to have this cell marked.
         board[boxIndex] = 'X'; //Used to check for win condition. by checking for 3 X's or 3 O's.
         player1TurnCount ++;  //counter for player 1's moves.
-
+        // document.getElementById('this').style.color = #999;
+        $(this).css("background-image", "linear-gradient(#00F260, #0575E6");
       } else {
 
         $(this).text('O'); // If its player O's turn place o in text field of This .cell clicked.
@@ -100,6 +64,7 @@ $(document).ready( function(){
         player2[boxIndex] = $(this)[0].id; //Change player2's array to have This .cell marked.
         board[boxIndex] = 'O'; // Used to check for win condition. byb checking for 3 X's or 3 O's.
         player2TurnCount ++; //Counter for player 2's moves.
+        $(this).css("background-image", "linear-gradient(90deg, #f12711, #f5af19");
         };
 
     turnCount = !turnCount //After either players turn, switch value so it is the other players turn.
@@ -151,6 +116,7 @@ $(document).ready( function(){
   $('#reset-button').on('click', function(){
 
     $('.cell').html('');
+    $('.cell').css('background-image', '');
 
     board = [
       '', '', '',
